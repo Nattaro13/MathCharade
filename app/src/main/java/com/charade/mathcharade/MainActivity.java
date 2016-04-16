53a82b6dc3f53dfea1838317c61979850a4f0bed
XueHui <nattaro13@gmail.com>
2016-04-16 23:15:33 +0800

    first commit

---
+++ app/src/main/java/com/charade/mathcharade/MainActivity.java
@@ -0,0 +1,49 @@
+package com.charade.mathcharade;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Run the game
+     * @param view
+     */
+    public void getGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Show users about the deck they are playing.
+     */
+    public void showInfo() {
+
+    }
+
+    /**
+    Executed when back button on the phone pad is pressed.
+    Brings user to home screen of their android phone
+    ie. EXIT APP.
+     */
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
