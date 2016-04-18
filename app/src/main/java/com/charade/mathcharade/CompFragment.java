69410b41380c1a5fc4ed649815db8ee1c5162b02
XueHui <nattaro13@gmail.com>
2016-04-18 23:43:18 +0800

    removed unnecessary functions in fragments

--- app/src/main/java/com/charade/mathcharade/CompFragment.java
+++ app/src/main/java/com/charade/mathcharade/CompFragment.java
@@ -18,14 +18,6 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CompFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-
-
-    // TODO: Rename and change types of parameters
-
-
 
     public CompFragment() {
         // Required empty public constructor
