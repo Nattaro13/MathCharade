88b0ad0849974c7f10f424390239cc44ad3eb754
XueHui <nattaro13@gmail.com>
2016-04-19 01:15:09 +0800

    implemented timer

--- app/src/main/java/com/charade/mathcharade/GameActivity.java
+++ app/src/main/java/com/charade/mathcharade/GameActivity.java
@@ -1,14 +1,31 @@
 package com.charade.mathcharade;
 
 import android.app.Activity;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends Activity {
+    private TextView mTextField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mTextField = (TextView) this.findViewById(R.id.game_timer);
+
+
+        CountDownTimer myTimer = new CountDownTimer(120000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                mTextField.setText("Time Left: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                mTextField.setText("Time's up!");
+                //get the results screen activity
+            }
+        }.start();
     }
 }
